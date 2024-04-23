309ed96effcf0557265aa101ad43e4be79d2dcd7
Sujit Sarkar <sujit_s@pursuitsoftware.biz>
2024-04-23 10:15:20 -0400

    explain results list change

--- explain_sql_results_gemini.py
+++ explain_sql_results_gemini.py
@@ -2,7 +2,7 @@ import google.generativeai as genai
 
 def explain_result (sql_prompt, sql_result):
   
-    user_prompt= f'Summarize the results from sql query in maximum four sentences. The result is an output from the following query {sql_prompt}. Result: {sql_result}'
+    user_prompt= f'If the {sql_prompt} is a asking for a list then do not Summarize. Otherwise Summarize the results from sql query in maximum 6 sentences. The result is an output from the following query {sql_prompt}. Result: {sql_result}. In the response do not mention database related words like SQL ,  Rows, TimeStamps  etc'
 
     models = genai.GenerativeModel('gemini-pro')
     response = models.generate_content(user_prompt)
